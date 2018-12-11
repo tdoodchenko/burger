fb4c7681eb1ef8307b535b273698871203513892
Taylor Doodchenko <taylordoodchenko@Taylors-MacBook-Pro.local>
2018-12-11 18:45:29 -0500

    error in Heroku deploy

--- config/orm.js
+++ config/orm.js
@@ -28,7 +28,7 @@ var orm = {
 
         connection.query(queryString, function(err, result) {
             if (err) {
-                throw err;
+                // throw err;
             }
             cb(result);
         });
